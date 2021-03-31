db443e43376a46b1867e31d8f603f71e2b5ec15e
ManojPriyaKasun <33677026+manojkasun@users.noreply.github.com>
2021-03-31 12:21:14 +0530

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//home screen
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeScreenComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { AddMessagesComponent } from './messages/add-messages/add-messages.component';
+import { ViewMessagesComponent } from './messages/view-messages/view-messages.component';
+import { UpdateMessageComponent } from './messages/update-message/update-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    AddMessagesComponent,
+    ViewMessagesComponent,
+    UpdateMessageComponent
   ],
   imports: [
     BrowserModule,
